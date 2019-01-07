c389ca06abc63ba6d97740743c623951786fb555
Nathan <nembery@gmail.com>
2019-01-07 17:36:15 -0500

    add bootstrap cdrom config

--- panos-vmware.tf
+++ panos-vmware.tf
@@ -26,25 +26,38 @@ variable vm_host_name {
   type = "string"
 }
 
+variable bootstrap_image {
+  default = '/var/tmp/panos-bootstrap.iso'
+  description = 'local path to an ISO image used for bootstraping the VM-Series'
+  type = 'string'
+}
+
 resource "vsphere_file" "panos-base-image" {
-  datacenter       = "dc1"
-  datastore        = "datastore1"
-  source_file      = "${var.panos_image}"
+  datacenter = "dc1"
+  datastore = "datastore1"
+  source_file = "${var.panos_image}"
   destination_file = "/base_images/panos-81-base.vmdk"
 }
 
-resource "vsphere_file" "ubuntu_disk_copy" {
+resource "vsphere_file" "panos-bootstrap-image" {
+  datacenter = "dc1"
+  datastore = "datastore1"
+  source_file = "${var.bootstrap_image}"
+  destination_file = "/bootstrap_images/${var.vm_host_name}-bootstrap.iso"
+}
+
+resource "vsphere_file" "vsphere_panos_image" {
   source_datacenter = "dc1"
-  datacenter        = "dc1"
-  source_datastore  = "datastore1"
-  datastore         = "datastore1"
-  source_file       = "/base_images/panos-81-base.vmdk"
-  destination_file  = "/instance_images/${var.vm_host_name}-disk1.vmdk"
+  datacenter = "dc1"
+  source_datastore = "datastore1"
+  datastore = "datastore1"
+  source_file = "/base_images/panos-81-base.vmdk"
+  destination_file = "/instance_images/${var.vm_host_name}-disk1.vmdk"
 }
 
 provider "vsphere" {
-  user           = "${var.vsphere_user}"
-  password       = "${var.vsphere_password}"
+  user = "${var.vsphere_user}"
+  password = "${var.vsphere_password}"
   vsphere_server = "${var.vsphere_server}"
 
   # If you have a self-signed cert
@@ -56,27 +69,27 @@ data "vsphere_datacenter" "dc" {
 }
 
 data "vsphere_datastore" "datastore" {
-  name          = "datastore1"
+  name = "datastore1"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
 data "vsphere_resource_pool" "pool" {
-  name          = "cluster1/Resources"
+  name = "cluster1/Resources"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
 data "vsphere_network" "network" {
-  name          = "VM Network"
+  name = "VM Network"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
 resource "vsphere_virtual_machine" "vm" {
-  name             = "${var.vm_host_name}"
+  name = "${var.vm_host_name}"
   resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
-  datastore_id     = "${data.vsphere_datastore.datastore.id}"
+  datastore_id = "${data.vsphere_datastore.datastore.id}"
 
   num_cpus = 4
-  memory   = 8192
+  memory = 8192
   guest_id = "centos64Guest"
 
   network_interface {
@@ -87,6 +100,11 @@ resource "vsphere_virtual_machine" "vm" {
     label = "disk0"
     attach = true
     path = "/instance_images/${var.vm_host_name}-disk1.vmdk"
-    datastore_id     = "${data.vsphere_datastore.datastore.id}"
+    datastore_id = "${data.vsphere_datastore.datastore.id}"
+  }
+
+  cdrom {
+    datastore_id = "${data.vsphere_datastore.datastore.id}"
+    path = "/bootstrap_images/${var.vm_host_name}-bootstrap.iso"
   }
 }
