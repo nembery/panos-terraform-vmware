70fc69ab7ef66225f971dfd65f30c2063340093b
Nathan <nembery@gmail.com>
2019-01-08 16:21:34 -0500

    update to use a cloned template

--- panos-vmware.tf
+++ panos-vmware.tf
@@ -1,3 +1,12 @@
+provider "vsphere" {
+  user = "${var.vsphere_user}"
+  password = "${var.vsphere_password}"
+  vsphere_server = "${var.vsphere_server}"
+
+  # If you have a self-signed cert
+  allow_unverified_ssl = true
+}
+
 variable vsphere_user {
   default = "user"
   description = "VSphere User"
@@ -16,8 +25,26 @@ variable vsphere_server {
   type = "string"
 }
 
+variable vcenter_datacenter {
+  default = "dc1"
+  description = "Name of the Datacenter"
+  type = "string"
+}
+
+variable vcenter_datastore {
+  default = "datastore1"
+  description = "Name of the Datastore"
+  type = "string"
+}
+
+variable vcenter_resoource_pool {
+  default = "rs1"
+  description = "Name of the Resource Pool"
+  type = "string"
+}
+
 variable panos_image {
-  default = "/var/tmp/PA-VM-ESX-8.1.0-disk1.vmdk"
+  default = "/base_images/PA-VM-300-ESX-8.1.0.vmdk"
 }
 
 variable vm_host_name {
@@ -27,59 +54,67 @@ variable vm_host_name {
 }
 
 variable bootstrap_image {
-  default = '/var/tmp/panos-bootstrap.iso'
-  description = 'local path to an ISO image used for bootstraping the VM-Series'
-  type = 'string'
+  default = "/var/tmp/panos-bootstrap.iso"
+  description = "local path to an ISO image used for bootstraping the VM-Series"
+  type = "string"
 }
 
-resource "vsphere_file" "panos-base-image" {
-  datacenter = "dc1"
-  datastore = "datastore1"
-  source_file = "${var.panos_image}"
-  destination_file = "/base_images/panos-81-base.vmdk"
+variable panos_template_vm {
+  default = "PA-VM-ESX-8.1.0"
+  description = "VM Instance to clone from"
+  type = "string"
 }
+
+//resource "vsphere_file" "panos-base-image" {
+//  datacenter = "${var.vcenter_datacenter}"
+//  datastore = "${var.vcenter_datastore}"
+//  source_file = "${var.panos_image}"
+//  destination_file = "/base_images/panos-81-base.vmdk"
+//  create_directories = true
+//}
 
 resource "vsphere_file" "panos-bootstrap-image" {
-  datacenter = "dc1"
-  datastore = "datastore1"
+  datacenter = "${var.vcenter_datacenter}"
+  datastore = "${var.vcenter_datastore}"
   source_file = "${var.bootstrap_image}"
   destination_file = "/bootstrap_images/${var.vm_host_name}-bootstrap.iso"
+  create_directories = true
 }
 
-resource "vsphere_file" "vsphere_panos_image" {
-  source_datacenter = "dc1"
-  datacenter = "dc1"
-  source_datastore = "datastore1"
-  datastore = "datastore1"
-  source_file = "/base_images/panos-81-base.vmdk"
-  destination_file = "/instance_images/${var.vm_host_name}-disk1.vmdk"
-}
+//resource "vsphere_file" "vsphere_panos_image" {
+//  source_datacenter = "${var.vcenter_datacenter}"
+//  datacenter = "${var.vcenter_datacenter}"
+//  source_datastore = "${var.vcenter_datastore}"
+//  datastore = "${var.vcenter_datastore}"
+//  source_file = "${var.panos_image}"
+//  destination_file = "/instance_images/${var.vm_host_name}-disk1.vmdk"
+//  create_directories = true
+//
+//  depends_on = ["vsphere_file.panos-base-image"]
+//}
 
-provider "vsphere" {
-  user = "${var.vsphere_user}"
-  password = "${var.vsphere_password}"
-  vsphere_server = "${var.vsphere_server}"
-
-  # If you have a self-signed cert
-  allow_unverified_ssl = true
-}
 
 data "vsphere_datacenter" "dc" {
-  name = "dc1"
+  name = "${var.vcenter_datacenter}"
 }
 
 data "vsphere_datastore" "datastore" {
-  name = "datastore1"
+  name = "${var.vcenter_datastore}"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
 data "vsphere_resource_pool" "pool" {
-  name = "cluster1/Resources"
+  name = "${var.vcenter_resoource_pool}"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
 data "vsphere_network" "network" {
   name = "VM Network"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+data "vsphere_virtual_machine" "panos_template" {
+  name          = "${var.panos_template_vm}"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
@@ -90,21 +125,33 @@ resource "vsphere_virtual_machine" "vm" {
 
   num_cpus = 4
   memory = 8192
-  guest_id = "centos64Guest"
+  guest_id = "${data.vsphere_virtual_machine.panos_template.guest_id}"
+  scsi_type = "${data.vsphere_virtual_machine.panos_template.scsi_type}"
+  wait_for_guest_net_timeout = 0
+  wait_for_guest_net_routable = 0
 
   network_interface {
     network_id = "${data.vsphere_network.network.id}"
   }
 
+  clone {
+    template_uuid = "${data.vsphere_virtual_machine.panos_template.id}"
+  }
+
   disk {
     label = "disk0"
-    attach = true
-    path = "/instance_images/${var.vm_host_name}-disk1.vmdk"
-    datastore_id = "${data.vsphere_datastore.datastore.id}"
+    size = "${data.vsphere_virtual_machine.panos_template.disks.0.size}"
+    eagerly_scrub    = "${data.vsphere_virtual_machine.panos_template.disks.0.eagerly_scrub}"
+    thin_provisioned = "${data.vsphere_virtual_machine.panos_template.disks.0.thin_provisioned}"
   }
 
   cdrom {
     datastore_id = "${data.vsphere_datastore.datastore.id}"
-    path = "/bootstrap_images/${var.vm_host_name}-bootstrap.iso"
+    path = "${vsphere_file.panos-bootstrap-image.destination_file}"
   }
+  depends_on = [
+    "vsphere_file.panos-bootstrap-image",
+//    "vsphere_file.panos-base-image",
+//    "vsphere_file.vsphere_panos_image"
+  ]
 }
